9f8e5d7cb05f04414eb451e1bc6880ae951c094b
HY. Kelvin Lee <34256578+hykelvinlee42@users.noreply.github.com>
2021-06-13 12:58:52 -0700

    Simulating the Teleportation Protocol with Statevector Simulator (#1)
    * statevector simulator
    
    * Refactored statevector simulator
    
    Co-authored-by: Shivansh <shivansh.sasan@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit.extensions import Initialize
+from qiskit import IBMQ, Aer, transpile, assemble
+from qiskit.visualization import plot_histogram, plot_bloch_multivector
 import matplotlib.pyplot as plt
 
 
@@ -14,6 +17,7 @@ def sender_gate(circuit, psi_qubit_id, sender_qubit_id):
 
 
 def meansure_and_send(circuit, psi_qubit_id, sender_qubit_id):
+    circuit.barrier()
     circuit.measure(psi_qubit_id, 0)  # measure and assign to first classical bit
     circuit.measure(sender_qubit_id, 1)  # measure and assign to second classical bit
 
@@ -28,24 +32,48 @@ def create_teleportation_circuit():
     quantum_register = QuantumRegister(3, name="q")
     classical_registerz = ClassicalRegister(1, name="crz")
     classical_registerx = ClassicalRegister(1, name="crx")
-    teleportation_circuit = QuantumCircuit(quantum_register, classical_registerx, classical_registerz)
+    teleportation_circuit = QuantumCircuit(quantum_register, classical_registerz, classical_registerx)
 
     # sender owns q0 and receiver owns q1
     sender_qubit_id = 1
     receiver_qubit_id = 2
     third_party_qubit_id = 0
 
+    psi = [0.45936609+0.56725257j, 0.40025413+0.55407937j]  # create qubit state
+    # plot_bloch_multivector(psi)
+    init_gate = Initialize(psi)
+    init_gate.label = "init"
+    inverse_init_gate = init_gate.gates_to_uncompute()
+
+    teleportation_circuit.append(init_gate, [0])
+    teleportation_circuit.barrier()
+
+    # The Quantum Teleportation Protocol, step 1
     entangle_qubits(teleportation_circuit, [sender_qubit_id, receiver_qubit_id])  # entangle qubits q0 and q1
 
+    # The Quantum Teleportation Protocol, step 2
     teleportation_circuit.barrier()
     sender_gate(teleportation_circuit, third_party_qubit_id, sender_qubit_id)
-    teleportation_circuit.barrier()
+
+    # The Quantum Teleportation Protocol, step 3
     meansure_and_send(teleportation_circuit, third_party_qubit_id, sender_qubit_id)
+
+    # The Quantum Teleportation Protocol, step 4
     teleportation_circuit.barrier()
     receiver_gate(teleportation_circuit, receiver_qubit_id, classical_registerz, classical_registerx)
 
+    # Statevector Simulator
+    statevector_simulation(teleportation_circuit)
+
     teleportation_circuit.draw(output="mpl")
     plt.show()
+
+
+def statevector_simulation(circuit):
+    sv_sim = Aer.get_backend("statevector_simulator")
+    qobj = assemble(circuit)
+    out_vector = sv_sim.run(qobj).result().get_statevector()
+    plot_bloch_multivector(out_vector)
 
 
 if __name__ == "__main__":
